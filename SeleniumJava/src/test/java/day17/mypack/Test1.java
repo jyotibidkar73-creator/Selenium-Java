4e01412bd9226ebd34497d26c670396cb00311b0
jyoti <jyotibidkar73@gmail.com>
2026-02-22 09:57:24 -0800

    Package &Access Modifier

---
+++ SeleniumJava/src/test/java/day17/mypack/Test1.java
@@ -0,0 +1,31 @@
+package day17.mypack;
+
+public class Test1 {
+
+	//private Access Modifier
+//	private int x =100;
+//	private void m1()
+//	{
+//		System.out.print("this is m1..");
+//	}
+	
+	//default modofier
+	
+	/*int x =100;
+	void m1()
+     {
+		System.out.print("this is m1..");
+    }
+	
+	*/
+	
+	//protected
+	protected int x =100;
+	protected void m1()
+     {
+		System.out.print("this is m1..");
+    }
+	
+	
+
+}
---
+++ SeleniumJava/src/test/java/day17/mypack2/Test2.java
@@ -0,0 +1,19 @@
+package day17.mypack2;
+
+import day17.mypack.Test1;
+
+public class Test2 extends Test1 {
+
+	public static void main(String[] args) {
+
+		Test2 t = new Test2();
+		System.out.println(t.x);
+		t.m1();
+		
+		/*Test1 t = new Test1();
+		System.out.println(t.x);
+		t.m1();
+		*/
+	}
+
+}
